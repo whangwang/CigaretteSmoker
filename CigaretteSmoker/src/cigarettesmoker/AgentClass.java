4d00d5addb357be1f01dc1f3757e59d381a49dbd
eric033014 <eric0330eric@gmail.com>
2017-10-17 02:03:14 +0800

    function ok

--- CigaretteSmoker/src/cigarettesmoker/AgentClass.java
+++ CigaretteSmoker/src/cigarettesmoker/AgentClass.java
@@ -10,5 +10,40 @@ package cigarettesmoker;
  * @author eric
  */
 public class AgentClass {
-    
+    private String Item1;
+    private String Item2;
+    public class agent extends Thread{
+       private String Material;
+       public agent(String material){
+           Material = material;
+       }
+       
+       public String Getame(){
+           return Material;
+       }
+       
+       public void run(){
+           while(true){
+ 
+               
+           }
+           
+       }
+       public synchronized void wake(){
+            try{
+                notify();
+            } catch(Exception e){}
+        }
+
+
+        public synchronized void pause(){
+            try{
+                this.wait();
+            } catch (InterruptedException e) {}
+        }
+               
+       
+    }
 }
+    
+
