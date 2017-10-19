5ea74ab4397d085a4eeb3449d7563b70c454cb4c
eric033014 <eric0330eric@gmail.com>
2017-10-19 13:20:11 +0800

    push table

---
+++ CigaretteSmoker/src/cigarettesmoker/TableClass.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cigarettesmoker;
+
+import static java.lang.Thread.sleep;
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
+
+/**
+ *
+ * @author eric
+ */
+
+public class TableClass {
+//    public static class table {
+        private Semaphore Notfull = new Semaphore(2);
+        private Semaphore Using = new Semaphore(1);
+        private ArrayList Items = new ArrayList();
+        public TableClass(){
+            System.out.println("Table class build success");
+            
+        }
+        public void run(String Material) throws InterruptedException{
+            Notfull.acquire();
+            Using.acquire();
+            Items.add(Material);
+            Using.release();
+            sleep(1000);
+                
+        }
+            
+        public void Release(){
+            Items.clear();
+            Notfull.release();
+        }
+        public boolean Isempty(){
+            return (Items.isEmpty());
+        }
+        public ArrayList Getitems(){
+            return (Items);
+        }
+        public synchronized void wake(){
+            try{
+                notify();
+            } catch(Exception e){}
+        }
+
+        public synchronized void pause(){
+            try{
+                this.wait();
+            } catch (InterruptedException e) {}
+        }
+            
+}
