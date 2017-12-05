17ea182712a8d324445841a2d2c5ea8aea3a713c
whangwang <whangwang0430@gmail.com>
2017-12-05 19:41:30 +0800

    front-end

--- CigaretteSmoker/src/cigarettesmoker/FXMLDocumentController.java
+++ CigaretteSmoker/src/cigarettesmoker/FXMLDocumentController.java
@@ -5,16 +5,19 @@
  */
 package cigarettesmoker;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Cursor;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
@@ -23,6 +26,8 @@ import javafx.scene.web.WebView;
  * @author HanWang
  */
 public class FXMLDocumentController implements Initializable {
+    
+    @FXML private AnchorPane rootPane;
     
     
     @FXML private WebView svgtest;
@@ -46,8 +51,10 @@ public class FXMLDocumentController implements Initializable {
     }
     
     @FXML
-    private void start_btn_click(MouseEvent event){
+    private void start_btn_click(MouseEvent event) throws IOException{
         System.out.println("start");  
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("Second.fxml"));
+        rootPane.getChildren().setAll(pane);
     }
     
     @Override
